b049016e40d783d3a8d9cae7f924733b7f5cf96f
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2023-01-05 22:55:46 +0800

    add navigate button

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { h, render } from "preact";
 
-const App = h("div", null, "Hello World");
+const Button = h("a", { href: "/#!/hashroute"}, "Go to hashbang");
+const App = h("div", null, Button);
+
 render(App, document.getElementById("app"));
